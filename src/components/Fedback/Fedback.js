3b4a2d491f2c2d0d87f71001e4e17cecf5384d24
VitaliySaburdo <vsaburdo@gmail.com>
2023-01-10 17:54:20 +0200

    rer

--- src/components/Fedback/Fedback.js
+++ src/components/Fedback/Fedback.js
@@ -2,10 +2,34 @@ import React from 'react';
 import { Container, Button, Box } from './Feedback.styled';
 
 class Feedback extends React.Component {
+  static defaultProps = {
+    initialGood: 0,
+    initialNeutral: 0,
+    initialBad: 0,
+  }
+  static propTypes = {
+    // 
+  }
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    good: this.props.initialGood,
+    neutral: this.props.initialNeutral,
+    bad: this.props.initialBad,
+  };
+
+  handleIncrementGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+  handleIncrementNetural = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+  handleIncrementBad = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
   };
 
   render() {
@@ -13,14 +37,20 @@ class Feedback extends React.Component {
       <Container>
         <h2>Please leave feedback</h2>
         <Box>
-          <Button type="button">Good</Button>
-          <Button type="button">Neutral</Button>
-          <Button type="button">Bad</Button>
+          <Button type="button" onClick={this.handleIncrementGood}>
+            Good
+          </Button>
+          <Button type="button" onClick={this.handleIncrementNetural}>
+            Neutral
+          </Button>
+          <Button type="button" onClick={this.handleIncrementBad}>
+            Bad
+          </Button>
         </Box>
         <h2>Statistics</h2>
-        <p>Good:</p>
-        <p>Netural:</p>
-        <p>Bad:</p>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
       </Container>
     );
   }
